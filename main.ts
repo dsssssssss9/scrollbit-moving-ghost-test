10e3cfc2449c3f8527710259b71a8533d5ebbeb0
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-09 18:19:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+scrollbit.clear()
+let cpos = 0
+basic.forever(function () {
+    scrollbit.clear()
+    scrollbit.setIcon(
+    IconNames.Ghost,
+    cpos,
+    0,
+    0
+    )
+    scrollbit.show()
+    basic.pause(500)
+    cpos += 1
+    if (cpos >= 13) {
+        cpos = 0
+    }
+})
